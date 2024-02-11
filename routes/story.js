2ff0316c3b3f05b72492a8febe9f9eab7b47ee32
GardRyan <res.gardner@gmail.com>
2024-02-11 11:30:09 -0500

    placed functions in routes originally - moved them to queries file. No changes to story.js

--- routes/story.js
+++ routes/story.js
@@ -1,24 +1,25 @@
 // all routes for showing and updating one story
 const express = require('express');
 const router  = express.Router();
+const getContributions = require ('../queries_contributions.js')
 
 router.get('/:id', (req, res) => {
   // get and return one story);
-}); 
+});
 
 router.get('/', (req, res) => {
   // get and return one story);
   res.render('story')
-}); 
+});
 
 router.post('/', (req, res) => {
   // create and insert one story
 
-}); 
+});
 
 router.post('/:id', (req, res) => {
   // update one story
-}); 
+});
 
 module.exports = router;
 
