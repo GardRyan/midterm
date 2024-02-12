edb654d60e265ffbe0c23cfd27127f901b54903b
GardRyan <res.gardner@gmail.com>
2024-02-12 15:14:15 -0500

    queries are rendering both stories and contirbutions

--- db/queries/queries_contributions.js
+++ db/queries/queries_contributions.js
@@ -1,10 +1,10 @@
 //require('dotenv').config();
-const db = require('../connection');
+const db = require("../connection");
 
 const getContributions = (storyId) => {
-let queryParams = [storyId];
-let queryString = `
-  SELECT * FROM contributions WHERE contributions.id = $1`
+  let queryParams = [storyId];
+  let queryString = `
+  SELECT * FROM contributions WHERE contributions.story_id = $1`;
   //   (SELECT COUNT(*)
   //     FROM contribution_votes as upvotes
   //     WHERE contributions.id = upvotes.contributions_id AND upvotes.vote = TRUE) AS upvotes
@@ -21,44 +21,23 @@ let queryString = `
   //   contributions.contributer_id,
   //   contributions.created_date,
   //   contributions.picked_date
-;
-
-  return db.query(queryString,queryParams)
-    .then(data => {
+  return db
+    .query(queryString, queryParams)
+    .then((data) => {
       console.log(data.rows);
       return data.rows;
     })
-    .catch ((error) => {
-      console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
 };
 
-const saveContributions = (newContribution) => {
-  const { story_id, story_step, content, picked, contributer_id, created_date, picked_date } = newContribution;
-  const query = 'INSERT INTO contributions (story_id, story_step, content, picked, contributer_id, created_date, picked_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
-
-  return db.query(query, [story_id, story_step, content, picked, contributer_id, created_date, picked_date])
-    .then((result) => {
-      return result.rows[0].id;
-    })
-    .catch(error => {
-      console.error('Error saving contribution:', error);
-    });
-};
-
-//to-do list
-//update contributions
-//pick contributions by picked attribute
-  //pick by story_id, picked = true
-  //order by story_step
-//get all contributions from unfinished step
-  //what is story_step
-  //select by story + story_Step
-  const getStoryById = () => {
-    let queryParams = [];
-    let queryString = `
+const getStoryById = (storyId) => {
+  let queryParams = [storyId];
+  let queryString = `
       SELECT *
       FROM stories
+      WHERE stories.id = $1
       GROUP BY
         stories.id,
         stories.title,
@@ -70,16 +49,55 @@ const saveContributions = (newContribution) => {
         stories.public,
         stories.deleted
     `;
-      return db.query(queryString,queryParams)
-        .then(data => {
-          if (data.rows.length === 0) {
-           throw new Error('story not found');
-          }
-          return data.rows;
-        })
-        .catch ((error) => {
-          console.log(error)
-        });
-    };
+  return db
+    .query(queryString, queryParams)
+    .then((data) => {
+      if (data.rows.length === 0) {
+        throw new Error("story not found");
+      }
+      return data.rows[0];
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-module.exports = { getContributions, saveContributions, getStoryById};
+const saveContributions = (newContribution) => {
+  const {
+    story_id,
+    story_step,
+    content,
+    picked,
+    contributer_id,
+    created_date,
+    picked_date,
+  } = newContribution;
+  const query =
+    "INSERT INTO contributions (story_id, story_step, content, picked, contributer_id, created_date, picked_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
+
+  return db
+    .query(query, [
+      story_id,
+      story_step,
+      content,
+      picked,
+      contributer_id,
+      created_date,
+      picked_date,
+    ])
+    .then((result) => {
+      return result.rows[0].id;
+    })
+    .catch((error) => {
+      console.error("Error saving contribution:", error);
+    });
+};
+//to-do list
+//update contributions
+//pick contributions by picked attribute
+//pick by story_id, picked = true
+//order by story_step
+//get all contributions from unfinished step
+//what is story_step
+//select by story + story_Step
+module.exports = { getContributions, saveContributions, getStoryById };
