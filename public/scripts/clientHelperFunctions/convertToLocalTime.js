b42da2bc57041ec6a73cb7fa1f8eaffeaef042fb
Jonas-Kunz <jonas_K_7@outlook.com>
2024-02-14 09:01:00 -0400

    branching commit

--- public/scripts/clientHelperFunctions/convertToLocalTime.js
+++ public/scripts/clientHelperFunctions/convertToLocalTime.js
@@ -1,15 +1,16 @@
-
 // converts to local time
 const convertToLocaleTime = function (date) {
-  const dateOptions = {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric'
-  };
+  if (date !== null) {
+    const dateOptions = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
 
-  const localDate = (new Date(date)).toLocaleTimeString(undefined, dateOptions);
-  return localDate
+    const localDate = new Date(date).toLocaleTimeString(undefined, dateOptions);
+    return localDate;
+  }
 
+  return null;
 };
-
