27f86376d3daac768b81ad761595bbda56d9edaf
GardRyan <res.gardner@gmail.com>
2024-02-15 23:23:42 -0500

    userId added to myStories, rerouted back to login without signing in. back up blank page for ejs

--- routes/stories.js
+++ routes/stories.js
@@ -2,21 +2,30 @@
 const express = require("express");
 const router = express.Router();
 const storiesQueries = require("../db/queries/stories");
-const { runWithLoginUser, renderErrorMessage, validRequestForUser, sendJsonErrorMessage } = require('./partials/_loginUser');
+const {
+  runWithLoginUser,
+  renderErrorMessage,
+  validRequestForUser,
+  sendJsonErrorMessage,
+} = require("./partials/_loginUser");
 
 //loads html skeleton
 router.get("/", (req, res) => {
   runWithLoginUser(req.session, req.session.user_id, (loginInfo) => {
-    res.render("stories", {loginInfo})
+    res.render("stories", { loginInfo });
   });
 });
 
 // loads page for my Stories
 router.get("/myStories", (req, res) => {
   runWithLoginUser(req.session, req.session.user_id, (loginInfo) => {
-    return res.redirect("/login")
+    const userId = req.session.user_id;
+    if (userId) {
+      res.render("myStories", { loginInfo, userId });
+    } else {
+      res.redirect("/login");
+    }
   });
 });
 
 module.exports = router;
-
